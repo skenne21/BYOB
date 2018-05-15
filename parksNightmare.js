6f9003dad943fe4c189cf0cdd615f778db25132d
Steven Lawson <unluckygamblermusic@gmail.com>
2018-05-15 12:33:15 -0600

    Add data to json files

--- parksNightmare.js
+++ parksNightmare.js
@@ -10,19 +10,23 @@ nightmareParksData
       const results = element.innerText.split('\t')
       const cleanResults = results.filter( res => res !== '')
       const cleanLocation  = cleanResults[1].split('\n')
+      const cleanSummary = cleanResults[5].replace(/[\[\]']+/g,'')
+      const removeNumbersSummary = cleanSummary.replace(/[0-9]/g, '')
+      console.log(cleanResults[5])
       return {
           name: cleanResults[0],
           location: cleanLocation[0],
           latLong: cleanLocation[1],
           date_open: cleanResults[2],
-          summary: cleanResults[5]
-      } 
+          summary: removeNumbersSummary
+      }
     })
     return [...stateNames];
   })
   .end()
   .then( results => {
-    let parks = JSON.stringify({ parks: results}, null, " ")
+    console.log(results)
+    let parks = JSON.stringify(results, null, " ")
     fs.writeFile('./parks-data.json', parks, "utf8", err => {
       if( err) {
         throw new Error('nope')
